936d9c19567d31b26bd4ed99b8976b9c9d3a60d6
Temitayo <temitayo_charles@yahoo.com>
2026-05-14 02:26:16 -0400

    Implement portfolio routing, SEO, accessibility, and case-study polish

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Portfolio from "@/components/portfolio/Portfolio";
+import CaseStudyPage from "@/components/portfolio/CaseStudyPage";
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Portfolio />} />
+          <Route path="/case/:projectId" element={<CaseStudyPage />} />
+          <Route path="/projects/:projectId" element={<CaseStudyPage />} />
+          <Route path="*" element={<Portfolio />} />
         </Routes>
       </BrowserRouter>
     </div>
