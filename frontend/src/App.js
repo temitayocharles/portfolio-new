e459aa77e68b13e275dff25338e554d16f9d31d4
Temitayo <temitayo_charles@yahoo.com>
2026-05-14 03:19:54 -0400

    Restore portfolio icon visuals and stabilize homepage mount

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,10 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Portfolio from "@/components/portfolio/Portfolio";
-import CaseStudyPage from "@/components/portfolio/CaseStudyPage";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Portfolio />} />
-          <Route path="/case/:projectId" element={<CaseStudyPage />} />
-          <Route path="/projects/:projectId" element={<CaseStudyPage />} />
-          <Route path="*" element={<Portfolio />} />
-        </Routes>
-      </BrowserRouter>
+      <Portfolio />
     </div>
   );
 }
